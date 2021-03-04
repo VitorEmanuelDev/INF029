fa8ab35ab815521d5b7e5c2fafa9563ed197cebb
vitoremanueldev <veslima3@gmail.com>
2021-03-04 12:42:52 -0300

    indentação

--- Atividades/AT02/menu.c
+++ Atividades/AT02/menu.c
@@ -10,54 +10,54 @@ int main(void){
 
     while(continua != 0){
 	    
-    puts("\nEscolha uma operação para dois números de sua escolha:\n");
-    puts("0 - Sair\n");
-    puts("1 - Somar\n");
-    puts("2 - Subtrair\n");
-    puts("3 - Multiplicar\n");
-    puts("4 - Dividir\n");
+        puts("\nEscolha uma operação para dois números de sua escolha:\n");
+        puts("0 - Sair\n");
+        puts("1 - Somar\n");
+        puts("2 - Subtrair\n");
+        puts("3 - Multiplicar\n");
+        puts("4 - Dividir\n");
     
-    scanf("%d", &input);
+        scanf("%d", &input);
 
-    if(input == 0){
+        if(input == 0){
     
-	continua = 0;
-        return 0;
+	    continua = 0;
+            return 0;
     
-    }
+        }
     
-    puts("\nEscolha dois inteiros:\n");
-    scanf("%f", &a);
-    scanf("%f", &b);
+        puts("\nEscolha dois inteiros:\n");
+        scanf("%f", &a);
+        scanf("%f", &b);
 
-         switch (input){
+        switch (input){
 
-            // case 0:
-            // return 0;
-            // break;
+           // case 0:
+           // return 0;
+           // break;
 
-             case 1 :
-             c = a + b;
-             printf ("%.2f", c);
-             break;
+            case 1 :
+            c = a + b;
+            printf ("%.2f", c);
+            break;
     
-             case 2 :
-             c = a - b;
-             printf ("%.2f", c);
-             break;
+            case 2 :
+            c = a - b;
+            printf ("%.2f", c);
+            break;
     
-             case 3 :
-             c = a * b;
-             printf ("%.2f", c);
-             break;
+            case 3 :
+            c = a * b;
+            printf ("%.2f", c);
+            break;
     
-             case 4 :
-             c = a / b;
-             printf ("%.2f", c);
-             break;
+            case 4 :
+            c = a / b;
+            printf ("%.2f", c);
+            break;
     
-             default :
-             printf ("Valor invalido!\n");
+            default :
+            printf ("Valor invalido!\n");
 
         }
 
