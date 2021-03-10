0f050130c41d6a4b917dc383cca73aac3011dfcc
vitoremanueldev <veslima3@gmail.com>
2021-03-10 03:30:19 -0300

    Correção da questão 09

--- Atividades/AT04/questao_09/Main.c
+++ Atividades/AT04/questao_09/Main.c
@@ -4,12 +4,20 @@
 #include <ctype.h>
 
 #define SIZE 25
+#define CPF 12
+
+typedef struct dma {
+  int dia;
+  int mes;
+  int ano; 
+  
+} Data; 
 
 typedef struct {
 
     char nome[SIZE];
-    int dia, mes, ano;
-    long long cpf;
+    Data data_nascimento;
+    char cpf[CPF];
     char sexo;
 
 }cliente;
@@ -24,15 +32,15 @@ int validarSexo(cliente verifica);
 int main(void){
 
     cliente perfil;
-
-    printf("Informe o nome, a data de nascimento (digite o dia, depois o mes e, por fim, o ano),\n o cpf (apenas digitos) e o sexo(M, F ou O):\n"); 
+    
+    
     
     perfil = cadastroCliente();
     
     printf("\n\nCadastro cliente.\n");
     printf("Nome: %s", perfil.nome);
-    printf("Nascimento: %d/%d/%d\n", perfil.dia, perfil.mes, perfil.ano);
-    printf("CPF: %lld\n", perfil.cpf);
+    printf("Nascimento: %d/%d/%d\n", perfil.data_nascimento.dia, perfil.data_nascimento.mes, perfil.data_nascimento.ano);
+    printf("CPF: %s\n", perfil.cpf);
     printf("Sexo: %c\n", toupper(perfil.sexo));
     
     return 0;
@@ -48,17 +56,21 @@ cliente cadastroCliente()
     int validaSexo;
     
     cliente verifica;
-
+    
+    printf("\nInforme o nome do cliente:\n");
     fgets(verifica.nome, SIZE, stdin);
     setbuf(stdin, NULL);
-    scanf("%d", &verifica.dia);
+    printf("\nInforme a data de nascimento do cliente (dd/mm/yyyy):\n");
+    scanf("%d", &verifica.data_nascimento.dia);
     setbuf(stdin, NULL);
-    scanf("%d", &verifica.mes);
+    scanf("%d", &verifica.data_nascimento.mes);
     setbuf(stdin, NULL);
-    scanf("%d", &verifica.ano);
+    scanf("%d", &verifica.data_nascimento.ano);
     setbuf(stdin, NULL);
-    scanf("%lld", &verifica.cpf);
+    printf("\nInforme o CPF do cliente:\n");
+    scanf(" %s", verifica.cpf);
     setbuf(stdin, NULL);
+    printf("\nInforme o sexo do cliente.\n");
     scanf("%c", &verifica.sexo);
     setbuf(stdin, NULL);
 
@@ -134,39 +146,97 @@ int validarSexo(cliente verifica) {
 
 int validarCPF(cliente verifica) {
 
+    char cpf[12];
+    int icpf[12];
+    int result1, result2, digito1, digito2, valor;
+      
+    int somador=0;
+    
+    strcpy(cpf, verifica.cpf);
   
-      if(verifica.cpf > 9999999999 && verifica.cpf <= 99999999999){
+    for(int i = 0; i < 11; i++){
+  
+        icpf[i] = cpf[i] - 48;
+  
+    }
     
-          return 1;
+     //encontrar o primeiro digito verificador
+    for(int i = 0; i < 9; i++)  {
+  
+        somador += icpf[i] * (10 - i); 
+       
+    }  
+    
+    result1 = somador % 11; 
+    
+    if( result1 == 0 || result1 == 1 ){
+      
+        digito1 = 0;  
         
-      }else {
+    }else{  
     
-          return -1;
+        digito1 = 11 - result1;  
+        
+    }  
+  
+    //encontrar o segundo digito verificador 
+  
+    somador = 0;  
+  
+    for(int i = 0; i < 10; i++)  {
+      
+        somador += icpf[i] * (11 - i);  
+        
+    }  
+  
+    valor = (somador / 11) * 11;  
     
-      }
+    result2 = somador - valor;  
+  
+    if(result2 == 0 || result2 == 1){
+      
+        digito2 = 0;
+          
+    }else{
+      
+        digito2 = 11 - result2;  
 
+    }  
+  
+ 
+  
+    if(digito1 == icpf[9] && digito2 == icpf[10]){  
+
+        return 1;
+    
+    }else{
+      
+        return -1;
+        
+    }
+    
 }
 
 
 int validarData(cliente verifica) {
     
-    if(verifica.ano >= 1900 && verifica.ano < 2004){
+    if(verifica.data_nascimento.ano >= 1900 && verifica.data_nascimento.ano < 2004){
     
-          if((verifica.dia >= 1 && verifica.dia <= 31) && 
-             (verifica.mes == 1 || verifica.mes == 3 || verifica.mes == 5 || verifica.mes == 7 || verifica.mes == 8 || verifica.mes == 10 || verifica.mes == 12)){
+          if((verifica.data_nascimento.dia >= 1 && verifica.data_nascimento.dia <= 31) && 
+             (verifica.data_nascimento.mes == 1 || verifica.data_nascimento.mes == 3 || verifica.data_nascimento.mes == 5 || verifica.data_nascimento.mes == 7 || verifica.data_nascimento.mes == 8 || verifica.data_nascimento.mes == 10 || verifica.data_nascimento.mes == 12)){
         
               return 1; 
         
-          }else if((verifica.dia >= 1 && verifica.dia <= 30) && 
-                 (verifica.mes == 4 || verifica.mes == 6 || verifica.mes == 9 || verifica.mes == 11)){
+          }else if((verifica.data_nascimento.dia >= 1 && verifica.data_nascimento.dia <= 30) && 
+                 (verifica.data_nascimento.mes == 4 || verifica.data_nascimento.mes == 6 || verifica.data_nascimento.mes == 9 || verifica.data_nascimento.mes == 11)){
                  
               return 1;         
                  
-          }else if((verifica.dia >= 1 && verifica.dia <= 28) && (verifica.mes == 2)){
+          }else if((verifica.data_nascimento.dia >= 1 && verifica.data_nascimento.dia <= 28) && (verifica.data_nascimento.mes == 2)){
           
               return 1;
           
-          }else if(verifica.dia == 29 && verifica.mes == 2 && (verifica.ano % 400 == 0 || (verifica.ano % 4 == 0 && verifica.ano % 100 != 0))){
+          }else if(verifica.data_nascimento.dia == 29 && verifica.data_nascimento.mes == 2 && (verifica.data_nascimento.ano % 400 == 0 || (verifica.data_nascimento.ano % 4 == 0 && verifica.data_nascimento.ano % 100 != 0))){
           
               return 1;
           
@@ -177,11 +247,11 @@ int validarData(cliente verifica) {
           }
     
     
-   }else{
+    }else{
    
        return -1;
    
-   }
+    }
 
 
 
---
+++ Escola/v01/projeto_escola_v01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct dados{
+
+char nome[100];
+char sexo; 
+
+}Aluno;
